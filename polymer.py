9290457b93678dde85cf486e7f4afd42ce4576aa
JStaa <jma@staa.nu>
2018-05-09 14:26:51 +0200

    Added rcm

--- polymer.py
+++ polymer.py
@@ -9,28 +9,25 @@ Nlist = [10, 50, 100, 500, 1000, 5000, 10000]
 meanlist = []
 
 def constantwalk(N):
-    crosslist = []
     rlist = []
     
     #This loop will generate mutliple siumlations
-    for i in range(0, 1000):    
+    for i in range(0, 1000):  
+        rcm = 0  
         xnext = 0
         ynext = 0
         xlist = [0]
         ylist = [0]
-        cross = 0
         
         #This loop generates the random walk
         for j in range(0, N):
             angle = 2*math.pi*random.random()
             xnext = math.cos(angle) + xlist[j]
-            ynext = math.sin(angle) + ylist[j]
-            if xnext == xlist[0] and ynext == ylist[0] and cross == 0:
-                cross = j       
+            ynext = math.sin(angle) + ylist[j]     
             xlist.append(xnext)
             ylist.append(ynext)
-        crosslist.append(cross)
-        
+            rcm += math.sqrt((xlist[-1] - xlist[0])**2 + (ylist[-1] - 
+                ylist[0])**2)
         #After every simulation the end-to-end distance is calculated
         r = math.sqrt((xlist[-1] - xlist[0])**2 + (ylist[-1] - ylist[0])**2)
         rlist.append(r)

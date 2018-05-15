d7161d10ca46fc13a662fb33829d03823bbf3953
JStaa <jma@staa.nu>
2018-05-15 11:13:03 +0200

    Added intersection alghoritm

--- polymer.py
+++ polymer.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,16 +9,26 @@ N = 10
 Nlist = [10, 50, 100, 500, 1000, 5000, 10000]
 meanlist = []
 
+##The following two functions will check if two line segments intersect based on 
+#their oriëntation.
+def ccw(Ax, Ay, Bx, By, Cx, Cy):
+    return (Cy-Ay)*(Bx-Ax) > (By-Ay)*(Cx-Ax)
+    
+def intersect(x0, y0, x1, y1, xprev, yprev, xnext, ynext):
+    return ccw(x0, y0, x1, y1, xprev, yprev) != ccw(x0, y0, x1, y1, xnext, 
+        ynext) and ccw(xprev, yprev, xnext, ynext, x0, y0) != ccw(xprev, yprev, 
+        xnext, ynext, x1, y1)
+
 def constantwalk(N):
     rlist = []
+    crosslist = []
     
-    #This loop will generate mutliple siumlations
+    #This loop will generate multiple siumlations
     for i in range(0, 1000):  
         rcm = 0  
-        xnext = 0
-        ynext = 0
         xlist = [0]
         ylist = [0]
+        cross = 0
         
         #This loop generates the random walk
         for j in range(0, N):
@@ -26,19 +37,29 @@ def constantwalk(N):
             ynext = math.sin(angle) + ylist[j]     
             xlist.append(xnext)
             ylist.append(ynext)
+            
+            ##This statement checks if a base pair intersected with the first
+            #base pair
+            if cross == 0 and j > 1 and intersect(0, 0, xlist[1], ylist[1], 
+            xlist[j], ylist[j], xnext, ynext) == True:
+                cross = j + 1
+            
             rcm += (1/N)*math.sqrt((xlist[-1] - xlist[0])**2 + (ylist[-1] - 
                 ylist[0])**2)
+                
         #After every simulation the end-to-end distance is calculated
         r = math.sqrt((xlist[-1] - xlist[0])**2 + (ylist[-1] - ylist[0])**2)
         rlist.append(r)
-        
+        if cross > 0:
+            crosslist.append(cross)
+    
     #The mean end-to-end distance is printed
-    return np.mean(rlist)
+    return np.mean(rlist), np.mean(crosslist)
 
 ##A plot is generated of the end-to-end distance as a function of base pair 
 #number
 for i in Nlist:
-    meanlist.append(constantwalk(i))
+    meanlist.append(constantwalk(i)[0])
     
 plt.figure(1)
 plt.clf()

71cbc85f3fc146b82024153538b366e1dd16b51a
JStaa <jma@staa.nu>
2018-05-15 12:27:52 +0200

    Added radius of gyration alghoritm

--- polymer.py
+++ polymer.py
@@ -7,7 +7,8 @@ import random
 #The number of base pairs
 N = 10
 Nlist = [10, 50, 100, 500, 1000, 5000, 10000]
-meanlist = []
+rmeanlist = []
+Rgmeanlist = []
 
 ##The following two functions will check if two line segments intersect based on 
 #their oriëntation.
@@ -22,13 +23,15 @@ def intersect(x0, y0, x1, y1, xprev, yprev, xnext, ynext):
 def constantwalk(N):
     rlist = []
     crosslist = []
+    Rglist = []
     
     #This loop will generate multiple siumlations
-    for i in range(0, 1000):  
-        rcm = 0  
+    for i in range(0, 1000): 
+        cross = 0 
+        rcm = 0
+        Rg = 0 
         xlist = [0]
         ylist = [0]
-        cross = 0
         
         #This loop generates the random walk
         for j in range(0, N):
@@ -44,24 +47,38 @@ def constantwalk(N):
             xlist[j], ylist[j], xnext, ynext) == True:
                 cross = j + 1
             
-            rcm += (1/N)*math.sqrt((xlist[-1] - xlist[0])**2 + (ylist[-1] - 
-                ylist[0])**2)
+            rcm += (1/float(N))*math.sqrt((xlist[-1])**2 + (ylist[-1])**2)
+        
+        for k in range(0, N):
+            Rg += (1/N+1)*(math.sqrt(xlist[k]**2 + ylist[k]**2) - rcm)**2
+        Rglist.append(Rg)
                 
         #After every simulation the end-to-end distance is calculated
         r = math.sqrt((xlist[-1] - xlist[0])**2 + (ylist[-1] - ylist[0])**2)
         rlist.append(r)
         if cross > 0:
             crosslist.append(cross)
-    
+        
     #The mean end-to-end distance is printed
-    return np.mean(rlist), np.mean(crosslist)
-
+    return np.mean(rlist), np.mean(crosslist), np.mean(Rglist)
+    
 ##A plot is generated of the end-to-end distance as a function of base pair 
 #number
 for i in Nlist:
-    meanlist.append(constantwalk(i)[0])
+    rmeanlist.append(constantwalk(i)[0])
+    Rgmeanlist.append(constantwalk(i)[2])
     
 plt.figure(1)
 plt.clf()
-plt.plot(Nlist, meanlist, 'b')
+plt.plot(Nlist, rmeanlist, 'b')
+plt.title('End-to-end distance as a function of base pairs')
+plt.xlabel('N')
+plt.ylabel('r')
+
+plt.figure(2)
+plt.clf()
+plt.plot(Nlist, Rgmeanlist, 'b')
+plt.title('Radius of gyration as a function of base pairs')
+plt.xlabel('N')
+plt.ylabel('$R_{g}$')
 plt.show()

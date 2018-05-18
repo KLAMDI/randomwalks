5c27a6439f48ddd88197ebb1e8c6c3e5669c5c36
JStaa <jma@staa.nu>
2018-05-18 12:29:06 +0200

    Added connective constant calculation

--- latticewalk.py
+++ latticewalk.py
@@ -6,50 +6,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-def latticewalk(latsize, Nsteps):
-    i = 0
-    
-    #A lattice array is created with all elements set to false
-    lattice = np.array([[False for x in xrange(latsize)] for y in 
-        xrange(latsize)])
-        
-    #The walk starts at the center of the lattice 
-    x = latsize/2
-    y = latsize/2
-    xlist = []
-    ylist = []
-    
-    #The walk must remain within the lattice
-    while ((x > 0) and (x < latsize-1) and (y > 0) and (y < latsize-1) and 
-    i <= Nsteps):
-        
-        #The current position is set to true
-        lattice[x][y] = True
-        xlist.append(x)
-        ylist.append(y)
-        
-        #If the walk reaches a dead end, the simulation is terminated
-        if (lattice[x-1][y] and lattice[x+1][y] and lattice[x][y-1] and 
-        lattice[x][y+1]):
-            break
-        rand = random.randrange(1, 5)
-        
-        #The walk can only move to points that have not yet been occupied
-        if   (rand == 1) and (not lattice[x+1][y]):
-            x += 1
-            i += 1
-        elif (rand == 2) and (not lattice[x-1][y]):
-            x -= 1
-            i += 1
-        elif (rand == 3) and (not lattice[x][y+1]):
-            y += 1
-            i += 1
-        elif (rand == 4) and (not lattice[x][y-1]):
-            y -= 1
-            i += 1
-    return (xlist, ylist)
+l = 100
+n = 10
 
-rlist = latticewalk(100, 100)    
+def latticewalk(latsize, Nsteps):
+    walkslist = []
+    for attempt in range(0, 4*3**(Nsteps-1)):
+        i = 0
+        movelist = []
+        
+        #A lattice array is created with all elements set to false
+        lattice = np.array([[False for x in xrange(latsize)] for y in 
+            xrange(latsize)])
+            
+        #The walk starts at the center of the lattice 
+        x = latsize/2
+        y = latsize/2
+        xlist = []
+        ylist = []
+        
+        #The walk must remain within the lattice
+        while ((x > 0) and (x < latsize-1) and (y > 0) and (y < latsize-1) and 
+        i <= Nsteps):
+            
+            #The current position is set to true
+            lattice[x][y] = True
+            xlist.append(x)
+            ylist.append(y)
+            
+            #If the walk reaches a dead end, the simulation is terminated
+            if (lattice[x-1][y] and lattice[x+1][y] and lattice[x][y-1] and 
+            lattice[x][y+1]):
+                break
+            rand = random.randrange(1, 5)
+            
+            #The walk can only move to points that have not yet been occupied
+            if   (rand == 1) and (not lattice[x+1][y]):
+                x += 1
+                movelist.append('r')
+                i += 1
+            elif (rand == 2) and (not lattice[x-1][y]):
+                x -= 1
+                movelist.append('l')
+                i += 1
+            elif (rand == 3) and (not lattice[x][y+1]):
+                y += 1
+                movelist.append('u')
+                i += 1
+            elif (rand == 4) and (not lattice[x][y-1]):
+                y -= 1
+                movelist.append('d')
+                i += 1
+        
+        del movelist[-1]
+        if movelist not in walkslist:        
+            walkslist.append(movelist)
+    return (xlist, ylist, len(walkslist))
+
+rlist = latticewalk(l, n) 
+print 'Approximate number of unique walk for %i steps: %i' % (n, rlist[2])
+print 'Approximate connective constant: %f' % (rlist[2]**(1/float(n)))
 plt.figure(1)
 plt.clf()
 plt.plot(rlist[0], rlist[1], 'b', rlist[0][0], rlist[1][0], 'go')
